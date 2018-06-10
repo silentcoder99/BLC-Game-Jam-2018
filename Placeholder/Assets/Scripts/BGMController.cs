4daf9e65927f827fb272a1541fb3c283bb8dfd76
silentcoder99 <silentcoder99@gmail.com>
2018-06-10 17:22:06 +1000

    Add background music

---
+++ Placeholder/Assets/Scripts/BGMController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour {
+    private static BGMController instance = null;
+    public static BGMController Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+}
