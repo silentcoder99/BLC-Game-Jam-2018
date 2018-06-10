691b05ea13848f564db65aaa999e8e9622717800
DESKTOP-6J852ML\Harry's Mac <harry.ellem94@gmail.com>
2018-06-10 17:30:24 +1000

    finished?

--- Placeholder/Assets/Scripts/DestroyByContact.cs
+++ Placeholder/Assets/Scripts/DestroyByContact.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DestroyByContact : MonoBehaviour
 {
     public AudioClip explosion;
-
+    public GameObject explosionParticle;
     private AudioSource audio;
 
     void OnTriggerEnter2D(Collider2D other)
@@ -18,15 +18,18 @@ public class DestroyByContact : MonoBehaviour
             //Play appropriate sound
             if (other.tag.Equals("Player"))
             {
+
                 audio.PlayOneShot(explosion);
             }
             //obstacles can only be destroyed by the boundary
             if (!other.gameObject.tag.Equals("Obstacle"))
             {
+                Instantiate(explosionParticle, other.transform.position, other.transform.rotation);
                 Destroy(other.gameObject);
             }
             if (!gameObject.tag.Equals("Obstacle"))
             {
+                Instantiate(explosionParticle, transform.position, transform.rotation);
                 Destroy(gameObject);
             }
         }
